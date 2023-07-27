686db96fc4317c3e9b173536d75d6477784c24d9
Matsvei Baranau <117320077+MatB1200@users.noreply.github.com>
2023-07-27 14:06:28 +0300

    Add files via upload

--- PE_server_scripts/Lua/ForcedAutorun/Sub_AutoDownload_and_Update.lua
+++ PE_server_scripts/Lua/ForcedAutorun/Sub_AutoDownload_and_Update.lua
@@ -1,12 +1,6 @@
--- Вписать ID кастомных лодок из мастерской.
--- пример(example): {"2808536198"}
-local workshopCollections = {}
-
--- Зазгрузка лодочек через .sub. 
-local path = "LocalMods/SubmarineDownloader/Downloaded/"
+ local path = "LocalMods/SubmarineDownloader/Downloaded/"
 
 if CLIENT then return end
-
 if not Game.IsDedicated then
     error("Ошибка, сервер не является выделенным")
     return
@@ -16,16 +10,15 @@ local function UpdateSubmarines()
     SubmarineInfo.RefreshSavedSubs()
     local toReplace = {}
 
-    for _, sub in pairs(SubmarineInfo.SavedSubmarines) do
-        if sub.Type == 0 then
-            table.insert(toReplace, sub)
+    for _, subm in pairs(SubmarineInfo.SavedSubmarines) do
+        if subm.Type == 0 then
+            table.insert(toReplace, subm)
         end
     end
 
     local files = File.DirSearch(path)
 
     local extension = ".sub"
-
     for key, value in pairs(files) do
         if value:sub(-#extension) == extension then
             local submarineInfo = SubmarineInfo(value)
@@ -42,57 +35,8 @@ local function UpdateSubmarines()
     end
 end
 
-local itemsBeingDownloaded = 0
-local itemsDownloaded = 0
+UpdateSubmarines()
 
-local function UpdateItem(id)
-    Steam.GetWorkshopItem(UInt64(id), function (item)
-        if item == nil then
-            print(string.format("Ошибка в поиске лодки id - %s.", id))
-            return
-        end
-
-        print(string.format("Скачивание последней версии '%s'...", item.Title))
-
-        Steam.DownloadWorkshopItem(item, path .. id, function (downloadedItem)
-            itemsDownloaded = itemsDownloaded + 1
-            print(string.format("(%s/%s) '%s' was успешно скачано %s", itemsDownloaded, itemsBeingDownloaded, downloadedItem.Title, path .. id))
-
-            if itemsDownloaded == itemsBeingDownloaded then
-                print("Обновление списка лодок.")
-                UpdateSubmarines()
-            end
-        end)
-    end)
-end
-
-local function UpdateAllItems()
-    if File.DirectoryExists(path) then
-        File.DeleteDirectory(path)
-    end
-
-    itemsBeingDownloaded = 0
-    itemsDownloaded = 0
-
-    for key, collection in pairs(workshopCollections) do
-        Steam.GetWorkshopItem(collection, function (item)
-            Steam.GetWorkshopCollection(collection, function (items)
-                print(string.format("Обновление %s библиотек для колекций '%s'", #items, item.Title))
-                for key, value in pairs(items) do
-                    UpdateItem(value)
-                    itemsBeingDownloaded = itemsBeingDownloaded + 1
-                end
-            end)
-        end)
-    end
-end
-
-Game.AddCommand("updateworkshop", "", function ()
-    UpdateAllItems()
+Game.AddCommand("UpdateSubmarines", "", function ()
+    UpdateSubmarines()
 end)
-
-if ExecutionNumber == 0 then -- перезагружает луа, и обновляет список
-    Timer.Wait(function ()
-        UpdateAllItems()
-    end, 5000)
-end
